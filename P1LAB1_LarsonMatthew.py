c4c79e8767473f640ba7b54ff515d81454782eda
mlarson444 <160437681+mlarson444@users.noreply.github.com>
2024-02-18 18:08:26 -0500

    Add files via upload

---
+++ P1LAB1_LarsonMatthew.py
@@ -0,0 +1,12 @@
+# Matthew Larson
+
+# 2Feb2024
+
+# P1LAB1
+
+# This project is to help familiarize us with IDLE and Python
+ 
+
+firstname = input("Enter first name:")
+print("Hi", firstname, end=' ')
+print('welcome to CTI-110-0902!')
