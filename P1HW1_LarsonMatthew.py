0058f14da282597a828a0e6b8d1a97f786d53ad0
mlarson444 <160437681+mlarson444@users.noreply.github.com>
2024-02-25 12:56:34 -0500

    Add files via upload
    Second Submission

---
+++ P1HW1_LarsonMatthew.py
@@ -0,0 +1,28 @@
+# Matthew Larson
+
+# 25Feb2024
+
+# P1HW1
+
+# This project is to help us understand using basic outputs with variables using IDLE
+
+
+user_num = int(input('Enter integer:\n'))
+
+
+print('You entered:', user_num)
+
+
+squared = user_num * user_num
+cubed = user_num * user_num * user_num
+print(user_num, 'squared is', squared)
+print('And', user_num, 'cubed is', cubed, '!', '!')
+
+
+user_num2 = int(input('Enter another integer:\n'))
+
+
+sum_result = user_num + user_num2
+product_result = user_num * user_num2
+print(user_num, '+', user_num2, 'is', sum_result)
+print(user_num, '*', user_num2, 'is', product_result)
