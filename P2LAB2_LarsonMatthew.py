e569217fb05abd75f9d4a65026d7683c876be0c6
mlarson444 <160437681+mlarson444@users.noreply.github.com>
2024-03-03 13:00:51 -0500

    Add files via upload
    This is my 6th submission

---
+++ P2LAB2_LarsonMatthew.py
@@ -0,0 +1,18 @@
+# Matthew Larson
+
+# 3 March 2024
+
+# P2LAB2
+
+# This helps us to calculate different averages for everyday tasks.
+
+num1=float(input())
+num2=float(input())
+num3=float(input())
+num4=float(input())
+
+product_all=num1*num2*num3*num4
+sum_all=num1+num2+num3+num4
+print()
+print("{:.0f} {:.0f}".format(product_all,sum_all/4))
+print("{:.3f} {:.3f}".format(product_all,sum_all/4))
